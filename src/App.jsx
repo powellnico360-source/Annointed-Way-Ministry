eb6510ea9aadfdc81fb35b6cad46fe347af09b14
powellnico360-source <powellnico360@gmail.com>
2026-04-23 10:10:55 +0300

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Annointed Way Ministry</h1>
+
+      <section>
+        <h2>Service Times</h2>
+        <p>8:00 - 10:00 Worshipping</p>
+        <p>10:00 - 11:00 Bible Study</p>
+        <p>11:00 - 12:00 Praise & Worship</p>
+        <p>12:00 - 1:00 Main Preaching</p>
+        <p>1:00 - 1:30 Closing Remarks</p>
+      </section>
+
+      <section>
+        <h2>Contact</h2>
+        <p>Bishop: 0720316052</p>
+        <p>Mrs Bishop: 0740169</p>
+        <p>Nairobi (Kibera)</p>
+      </section>
+
+    </div>
+  );
+} 
